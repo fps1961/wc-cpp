a4eeb6577235e5298fffd274eb0ad1713b2cde52
FruitPunchSamurai <sheshan1961@gmail.com>
2026-05-14 22:48:39 -0400

    Refactor code and implement -l option
    User can now use -l [file_path] to get the total number of lines for a file

--- main.cpp
+++ main.cpp
@@ -4,15 +4,43 @@
 #include <iostream>
 
 
-void printTotalBytesOfFile(const std::string& file_path)
+void checkIfFileProvided(const int argc)
+{
+    if (argc <= 2)
+    {
+        std::cerr << "No file provided.\n";
+        exit(1);
+    }
+};
+
+void checkIfFilePathIsValid(const std::string& file_path)
 {
     if (!std::filesystem::exists(file_path))
     {
-        std::cout << std::format("No file found with path {}", file_path);
-        return;
+        std::cerr << std::format("No file found with path {}", file_path);
+        exit(1);
+    }
+}
+
+
+void printTotalBytesOfFile(const std::string& file_path)
+{
+    std::cout << std::format("{} {}\n", std::filesystem::file_size(file_path), file_path);
+}
+
+void printTotalLinesOfFile(const std::string& file_path)
+{
+    std::ifstream file_stream(file_path);
+
+    std::string line{};
+    long long total_lines{0};
+
+    while (std::getline(file_stream, line))
+    {
+        ++total_lines;
     }
 
-    std::cout << std::format("{} {}\n", std::filesystem::file_size(file_path), file_path);
+    std::cout << std::format("{} {}\n", total_lines, file_path);
 }
 
 
@@ -29,19 +57,20 @@ int main(const int argc, char* argv[])
 
     if (const auto command = std::string(argv[1]); command == "-c")
     {
-        if (argc == 2)
-        {
-            std::cerr << "No file provided.\n";
-            exit(1);
-        }
-
+        checkIfFileProvided(argc);
         printTotalBytesOfFile(argv[2]);
+    }
+    else if (command == "-l")
+    {
+        checkIfFileProvided(argc);
+        printTotalLinesOfFile(argv[2]);
     }
     else if (command == "-h")
     {
-        std::string help_message_for_c_option = "-c [file] will return the number of bytes in the file.";
+        std::string c_option = "-c [file_path] will return the number of bytes in the file.";
+        std::string l_option = "-l [file_path] will return the number of lines in the file.";
 
-        std::cout << std::format("{}\n", help_message_for_c_option);
+        std::cout << std::format("{}\n{}\n", c_option, l_option);
     }
     else
     {
